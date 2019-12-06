c51cf99d0e3b11abb8b14ca5c8d62947fdc5bfc3
Jake Goulding <jake.goulding@gmail.com>
2019-12-05 21:48:49 -0500

    Day 1, part 2

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,5 +1,5 @@
 fn mass_to_fuel(mass: u32) -> u32 {
-    (mass / 3) - 2
+    (mass / 3).saturating_sub(2)
 }
 
 #[test]
@@ -10,12 +10,32 @@ fn specifications() {
     assert_eq!(mass_to_fuel(100756), 33583);
 }
 
+fn mass_to_fuel_recursive(mass: u32) -> u32 {
+    std::iter::successors(Some(mass), |&mass| {
+        let f = mass_to_fuel(mass);
+        if f > 0 {
+            Some(f)
+        } else {
+            None
+        }
+    })
+    .skip(1) // Ignore the initial mass
+    .sum()
+}
+
+#[test]
+fn specifications2() {
+    assert_eq!(mass_to_fuel_recursive(14), 2);
+    assert_eq!(mass_to_fuel_recursive(1969), 966);
+    assert_eq!(mass_to_fuel_recursive(100756), 50346);
+}
+
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
     let sum = INPUT
         .lines()
-        .map(|l| l.parse().map(mass_to_fuel))
+        .map(|l| l.parse().map(mass_to_fuel_recursive))
         .sum::<Result<u32, _>>()
         .expect("Unable to compute sum");
 
