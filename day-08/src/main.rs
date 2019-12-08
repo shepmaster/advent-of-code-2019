be887baaad72ce64937d184b51563fdc23aa34de
Jake Goulding <jake.goulding@gmail.com>
2019-12-08 16:40:28 -0500

    Day 8, part 2

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -1,15 +1,21 @@
+use itertools::Itertools;
 use std::collections::BTreeMap;
 
 pub type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
-fn checksum(data: &str, width: usize, height: usize) -> Result<usize> {
+fn layers(data: &str, width: usize, height: usize) -> Result<Vec<Vec<usize>>> {
     let mut digits = data.split("").flat_map(str::parse).peekable();
 
     let mut layers = Vec::new();
     while digits.peek().is_some() {
         layers.push(digits.by_ref().take(width * height).collect::<Vec<usize>>());
     }
+    Ok(layers)
+}
+
+fn checksum(data: &str, width: usize, height: usize) -> Result<usize> {
+    let layers = layers(data, width, height)?;
 
     let counts: Vec<_> = layers
         .iter()
@@ -33,15 +39,58 @@ fn checksum(data: &str, width: usize, height: usize) -> Result<usize> {
     Ok(ones * twos)
 }
 
+fn composite(data: &str, width: usize, height: usize) -> Result<String> {
+    let layers = layers(data, width, height)?;
+    let composed_layer = layers
+        .into_iter()
+        .fold1(|comp, layer| {
+            comp.into_iter()
+                .zip(layer)
+                .map(|(c, l)| match c {
+                    2 => l,
+                    _ => c,
+                })
+                .collect()
+        })
+        .ok_or("Did not have any layers")?;
+
+    let mut layer_string = String::new();
+    for line in composed_layer.chunks(width) {
+        use std::fmt::Write;
+        for n in line {
+            write!(&mut layer_string, "{}", n)?;
+        }
+        writeln!(&mut layer_string)?;
+    }
+    Ok(layer_string)
+}
+
 #[test]
-fn specifications() -> Result<()> {
+fn specifications_checksum() -> Result<()> {
     let data = "123456789012";
     assert_eq!(checksum(data, 3, 2)?, 1);
     Ok(())
 }
 
+#[test]
+fn specifications_composite() -> Result<()> {
+    let data = "0222112222120000";
+    assert_eq!(composite(data, 2, 2)?, "01\n10\n");
+    Ok(())
+}
+
 const INPUT: &str = include_str!("input.txt");
 
-fn main() {
-    println!("{:?}", checksum(INPUT, 25, 6));
+fn main() -> Result<()> {
+    println!("{}", checksum(INPUT, 25, 6)?);
+
+    let composite = composite(INPUT, 25, 6)?;
+    for mut c in composite.chars() {
+        if c == '0' {
+            c = ' '
+        }
+        print!("{}", c);
+    }
+
+    Ok(())
 }
