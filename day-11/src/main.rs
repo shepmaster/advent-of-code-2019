95756b2a391c35cc4d6fdfcef00ab75861cf10d1
Jake Goulding <jake.goulding@gmail.com>
2019-12-11 22:43:50 -0500

    Day 11, part 2

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -2,10 +2,12 @@ use itertools::Itertools;
 use std::{collections::BTreeMap, thread};
 
 const BLACK: intcode::Byte = 0;
+const WHITE: intcode::Byte = 1;
 
 const TURN_LEFT: intcode::Byte = 0;
 
 type Coord = (i32, i32);
+type Hull = BTreeMap<Coord, intcode::Byte>;
 
 #[derive(Debug, Copy, Clone)]
 enum Direction {
@@ -44,7 +46,15 @@ impl Direction {
     }
 }
 
-fn paint(mut program: intcode::Program) -> usize {
+fn painted_squares(program: intcode::Program) -> usize {
+    paint_common(program, BLACK).len()
+}
+
+fn painted_hull(program: intcode::Program) -> Hull {
+    paint_common(program, WHITE)
+}
+
+fn paint_common(mut program: intcode::Program, initial_square: intcode::Byte) -> Hull {
     let (tx, rx) = intcode::channel();
     let (tx2, rx2) = intcode::channel();
 
@@ -54,6 +64,8 @@ fn paint(mut program: intcode::Program) -> usize {
         let mut hull = BTreeMap::new();
         let mut position = (0, 0);
         let mut direction = Up;
+
+        hull.insert(position, initial_square);
 
         let mut rx = rx.into_iter().tuples();
 
@@ -83,13 +95,44 @@ fn paint(mut program: intcode::Program) -> usize {
         .expect("Execution failed");
     let hull = painter.join().expect("Painter panicked");
 
-    hull.len()
+    hull
 }
 
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
     let program: Vec<_> = INPUT.trim().split(",").flat_map(str::parse).collect();
-    let painted_squares = paint(program);
+
+    let painted_squares = painted_squares(program.clone());
     println!("{}", painted_squares);
+
+    let painted_hull = painted_hull(program);
+    display_hull(&painted_hull);
+}
+
+fn display_hull(hull: &Hull) {
+    let (min_x, max_x) = hull
+        .keys()
+        .copied()
+        .map(|(x, _)| x)
+        .minmax()
+        .into_option()
+        .expect("Nothing painted");
+    let (min_y, max_y) = hull
+        .keys()
+        .copied()
+        .map(|(_, y)| y)
+        .minmax()
+        .into_option()
+        .expect("Nothing painted");
+
+    for y in (min_y..=max_y).rev() {
+        for x in min_x..=max_x {
+            match hull.get(&(x, y)) {
+                Some(&WHITE) => print!("█"),
+                _ => print!(" "),
+            }
+        }
+        println!();
+    }
 }
